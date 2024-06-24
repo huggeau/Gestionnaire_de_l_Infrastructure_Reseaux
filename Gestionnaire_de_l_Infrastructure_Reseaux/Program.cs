8aee733b4606855511d8966f0d5c234c30a5c9c9
hugo goisot <hugo.goisot@lyceeastier.com>
2024-06-24 16:55:01 +0200

    version avec test de connexion a la bdd

--- Gestionnaire_de_l_Infrastructure_Reseaux/Program.cs
+++ Gestionnaire_de_l_Infrastructure_Reseaux/Program.cs
@@ -1,3 +1,6 @@
+using Gestionnaire_de_l_Infrastructure_Reseaux.métier;
+using MySql.Data.MySqlClient;
+
 namespace Gestionnaire_de_l_Infrastructure_Reseaux
 {
     internal static class Program
@@ -6,12 +9,30 @@ namespace Gestionnaire_de_l_Infrastructure_Reseaux
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(String[] args)
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new FenetrePrincipale());
+
+            Console.WriteLine("Getting Connection ...");
+            MySqlConnection conn = BDDUtils.GetDBConnection();
+
+            try
+            {
+                Console.WriteLine("Openning Connection ...");
+
+                conn.Open();
+
+                Console.WriteLine("Connection successful!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+
+            Console.Read();
         }
     }
 }
---
+++ Gestionnaire_de_l_Infrastructure_Reseaux/métier/BDDUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Gestionnaire_de_l_Infrastructure_Reseaux.métier
+{
+    internal class BDDUtils
+    {
+        public static MySqlConnection GetDBConnection()
+        {
+            string host = "192.168.10.145";
+            int port = 3306;
+            string database = "db23_glpi";
+            string username = "root";
+            string password = "Admin07200&";
+
+            return BDDMySQLUtils.GetDBConnection(host, port, database, username, password);
+        }
+    }
+}
