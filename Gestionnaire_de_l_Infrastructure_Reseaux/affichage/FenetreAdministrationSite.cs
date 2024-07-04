9496a9dc937442f298fe285bec01cfe42a0b9818
hugo goisot <hugo.goisot@lyceeastier.com>
2024-07-04 10:07:14 +0200

    version avec modificiation de la BDD en fonction es données rentré dans  la fentre d'administration et avec en plus des combox permettant de choisir l'étage et la catégorie

--- Gestionnaire_de_l_Infrastructure_Reseaux/affichage/FenetreAdministrationSite.cs
+++ Gestionnaire_de_l_Infrastructure_Reseaux/affichage/FenetreAdministrationSite.cs
@@ -1,4 +1,5 @@
 ﻿using Gestionnaire_de_l_Infrastructure_Reseaux.métier;
+using Mysqlx.Crud;
 using Mysqlx.Resultset;
 using System;
 using System.Collections;
@@ -10,6 +11,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 
 namespace Gestionnaire_de_l_Infrastructure_Reseaux
 {
@@ -22,6 +24,7 @@ namespace Gestionnaire_de_l_Infrastructure_Reseaux
         private TableLayoutPanel tableLayoutPanel = new TableLayoutPanel();
         private List<TextBox> listTextBox = new List<TextBox>();
         private List<string> listIps = new List<string>();
+        private List<ComboBox> listComboBox = new List<ComboBox>();
 
 
         public FenetreAdministrationSite(int id)
@@ -42,6 +45,10 @@ namespace Gestionnaire_de_l_Infrastructure_Reseaux
             panel1.Controls.Add(tableLayoutPanel);
 
             LoadDataAndPopulateTable();
+
+            for (int i = 0; i < listTextBox.Count; i+=5) { 
+                listTextBox[i].Enabled = false;
+            }
         }
 
         private void ajouterToolStripMenuItem_Click(object sender, EventArgs e)
@@ -81,7 +88,7 @@ namespace Gestionnaire_de_l_Infrastructure_Reseaux
                 try
                 {
                     conn.Open();
-                    string query = $"SELECT mr.ip,  mr.nom, e.nom AS etage,  cm.nom AS categorie_de_materiel, commentaire, emplacement FROM  Materiel_Reseau mr " +
+                    string query = $"SELECT mr.id, mr.ip,  mr.nom, e.nom AS etage,  cm.nom AS categorie_de_materiel, commentaire, emplacement FROM  Materiel_Reseau mr " +
                         $"INNER JOIN  etage e ON mr.id_etage = e.id " +
                         $"INNER JOIN   categorie_de_materiel cm ON mr.id_categorie_de_materiel = cm.id" +
                         $" WHERE mr.id_site = {idSite};";
@@ -124,24 +131,55 @@ namespace Gestionnaire_de_l_Infrastructure_Reseaux
             {
                 for (int col = 0; col < dataTable.Columns.Count; col++)
                 {
-                    TextBox cellTextBox = new TextBox
+                    if (col == 3) 
                     {
-                        Text = dataTable.Rows[row][col].ToString(),
-                        Dock = DockStyle.Fill,
-                        TextAlign = HorizontalAlignment.Center,
-                        Size = new System.Drawing.Size(10, 50),
-                        BorderStyle = BorderStyle.FixedSingle
-                    };
+                        ComboBox comboBox = new ComboBox
+                        {
+                            Text = dataTable.Rows[row][col].ToString(),
+                            Dock = DockStyle.Fill,
+                        };
+                        RemplirComboBoxEtage(comboBox);
+
+                        listComboBox.Add(comboBox);
+
+                        tableLayoutPanel.Controls.Add(comboBox);
+                        
+                    }
+                    else if (col == 4)
+                    {
+                        ComboBox comboBox = new ComboBox
+                        {
+                            Text = dataTable.Rows[row][col].ToString(),
+                            Dock = DockStyle.Fill,
+                        };
+                        RemplirComboBoxCategorie(comboBox);
+
+                        listComboBox.Add(comboBox);
+
+                        tableLayoutPanel.Controls.Add(comboBox);
+                    }
+                    else 
+                    { 
+                        TextBox cellTextBox = new TextBox
+                        {
+                            Text = dataTable.Rows[row][col].ToString(),
+                            Dock = DockStyle.Fill,
+                            TextAlign = HorizontalAlignment.Center,
+                            Size = new System.Drawing.Size(10, 50),
+                            BorderStyle = BorderStyle.FixedSingle
+                        };
+
                     tableLayoutPanel.Controls.Add(cellTextBox, col, row + 1);
 
                     listTextBox.Add(cellTextBox);
+                    }
                 }
             }
         }
 
         private void Ping()
         {
-            int IndexLabel = 0;
+            int IndexLabel = 1;
             for (int i = 0; i < listIps.Count; i++)
             {
 
@@ -154,42 +192,116 @@ namespace Gestionnaire_de_l_Infrastructure_Reseaux
                     listTextBox[IndexLabel].BackColor = Color.Red;
                 }
 
-                IndexLabel += 6;
+                IndexLabel += 5;
             }
         }
-        
+
         private void UpdateInfoBDD()
         {
             using (var connection = new MySqlConnector.MySqlConnection(comm.connexionBDD()))
             {
                 connection.Open();
-                List<int> siteIds = comm.RemplirListSite();
+                int indexTextBox = 0;
+                int indexComboBox = 0;
                 // Insert new position
-                for (int i = 0; i < siteIds.Count; i++)
+                for (int i = 0; i < tableLayoutPanel.RowCount; i++)
                 {
-                    int siteId = siteIds[i];
-                    string updateQuery = "UPDATE Materiel_Reseau SET ip = @ip, nom = @nom, commentaire = @commentaire, emplacement = @emplacement WHERE id = @id";
+                    string updateQuery = "UPDATE Materiel_Reseau mr " +
+                                         "INNER JOIN etage e ON mr.id_etage = e.id " +
+                                         "INNER JOIN categorie_de_materiel cm ON mr.id_categorie_de_materiel = cm.id " +
+                                         "SET mr.ip = @ip, " +
+                                         "mr.nom = @nom, " +
+                                         "mr.commentaire = @commentaire, " +
+                                         "mr.emplacement = @emplacement, " +
+                                         "mr.id_etage = @id_etage, " +
+                                         "mr.id_categorie_de_materiel = @id_categorie " +
+                                         "WHERE mr.id = @id";
 
                     using (var command = new MySqlConnector.MySqlCommand(updateQuery, connection))
                     {
                         try
                         {
-                            command.Parameters.AddWithValue("@ip", listTextBox[i].Text);
-                            command.Parameters.AddWithValue("@nom", listTextBox[i+1].Text);
-                            command.Parameters.AddWithValue("@commentaire", listTextBox[i+4].Text);
-                            command.Parameters.AddWithValue("@emplacement", listTextBox[i + 5].Text);
-                            command.Parameters.AddWithValue("@id", idSite);
+                            int id = Convert.ToInt32(listTextBox[indexTextBox].Text);
+                            command.Parameters.AddWithValue("@ip", listTextBox[indexTextBox + 1].Text);
+                            command.Parameters.AddWithValue("@nom", listTextBox[indexTextBox + 2].Text);
+                            command.Parameters.AddWithValue("@commentaire", listTextBox[indexTextBox + 3].Text);
+                            command.Parameters.AddWithValue("@emplacement", listTextBox[indexTextBox + 4].Text);
+                            try
+                            {
+                                var selectedEtage = listComboBox[indexComboBox].SelectedItem as ComboBoxItem;
+                                var selectedCategorie = listComboBox[indexComboBox + 1].SelectedItem as ComboBoxItem;
+                                if (selectedEtage != null && selectedCategorie != null)
+                                {
+                                    command.Parameters.AddWithValue("@id_etage", selectedEtage.Id);
+                                    command.Parameters.AddWithValue("@id_categorie", selectedCategorie.Id);
+                                }
+                                else
+                                {
+                                    throw new Exception("Sélection invalide dans les ComboBox.");
+                                }
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine(e.ToString());
+                            }
+                            command.Parameters.AddWithValue("@id", id);
                             command.ExecuteNonQuery();
+
+                            indexTextBox += 5;
+                            indexComboBox += 2;
                         }
                         catch (Exception ex)
                         {
-
                             Console.WriteLine("Error: " + ex.Message);
                         }
                     }
                 }
-
             }
+        }
+        public void RemplirComboBoxEtage(ComboBox comboBox)
+        {
+            string query = "SELECT id, nom FROM etage";
+
+            using (MySqlConnector.MySqlConnection connection = new MySqlConnector.MySqlConnection(comm.connexionBDD()))
+            {
+                MySqlConnector.MySqlCommand command = new MySqlConnector.MySqlCommand(query, connection);
+                connection.Open();
+                MySqlConnector.MySqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    int id = reader.GetInt32(0);
+                    string name = reader.GetString(1);
+
+                    comboBox.Items.Add(new ComboBoxItem(id, name));
+                }
+                reader.Close();
+            }
+            comboBox.DisplayMember = "Name"; // Propriété à afficher
+            comboBox.ValueMember = "Id"; // Valeur utilisée en interne
+        }
+
+        public void RemplirComboBoxCategorie(ComboBox comboBox)
+        {
+            string query = "SELECT id, nom FROM categorie_de_materiel";
+
+            using (MySqlConnector.MySqlConnection connection = new MySqlConnector.MySqlConnection(comm.connexionBDD()))
+            {
+                MySqlConnector.MySqlCommand command = new MySqlConnector.MySqlCommand(query, connection);
+                connection.Open();
+                MySqlConnector.MySqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    int id = reader.GetInt32(0);
+                    string name = reader.GetString(1);
+
+                    comboBox.Items.Add(new ComboBoxItem(id, name));
+                }
+                reader.Close();
+            }
+            comboBox.DisplayMember = "Name"; // Propriété à afficher
+            comboBox.ValueMember = "Id"; // Valeur utilisée en interne
         }
     }
 }
---
+++ Gestionnaire_de_l_Infrastructure_Reseaux/métier/ComboBoxItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestionnaire_de_l_Infrastructure_Reseaux.métier
+{
+    internal class ComboBoxItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ComboBoxItem(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name; // Cela permettra d'afficher le nom dans la ComboBox
+        }
+    }
+}
