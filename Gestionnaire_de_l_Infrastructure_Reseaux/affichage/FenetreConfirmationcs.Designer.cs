8bddefc7d983736ce8e8e31a273f599bed87901a
hugo goisot <hugo.goisot@lyceeastier.com>
2024-07-04 16:29:57 +0200

    fenetre pour supprimer un materiel fini

---
+++ Gestionnaire_de_l_Infrastructure_Reseaux/affichage/FenetreConfirmationcs.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Gestionnaire_de_l_Infrastructure_Reseaux.affichage
+{
+    partial class FenetreConfirmationcs
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 82);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 0;
+            button1.Text = "Oui";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(233, 82);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 1;
+            button2.Text = "Non";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 26);
+            label1.Name = "label1";
+            label1.Size = new Size(330, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Êtes-vous sûr de vouloir supprimer cet élément ?\r\n";
+            // 
+            // FenetreConfirmationcs
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(353, 134);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "FenetreConfirmationcs";
+            Text = "fenetre de confirmation ";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Label label1;
+    }
+}
--- Gestionnaire_de_l_Infrastructure_Reseaux/affichage/FenetreSupprimerMateriel.cs
+++ Gestionnaire_de_l_Infrastructure_Reseaux/affichage/FenetreSupprimerMateriel.cs
@@ -1,4 +1,5 @@
-﻿using Gestionnaire_de_l_Infrastructure_Reseaux.métier;
+﻿using Gestionnaire_de_l_Infrastructure_Reseaux.affichage;
+using Gestionnaire_de_l_Infrastructure_Reseaux.métier;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -50,7 +51,41 @@ namespace Gestionnaire_de_l_Infrastructure_Reseaux
 
         private void BoutonSupprimer_Click(object sender, EventArgs e)
         {
+            FenetreConfirmationcs fenetreConfirmationcs = new FenetreConfirmationcs();
+            DialogResult result = fenetreConfirmationcs.ShowDialog();
 
+            if (result == DialogResult.OK)
+            {
+                MessageBox.Show("élément supprimer de la base de données ");
+            }
+            else
+            {
+                MessageBox.Show("élément non-supprimer de la base de données");
+            }
+        }
+
+        private void SupprimerElement()
+        {
+            int id;
+            var selectedId = comboBox1.SelectedItem as ComboBoxItem;
+            id = selectedId.Id;
+            string query = $"DELETE FROM Materiel_Reseau WHERE id = {id}";
+
+            using (MySqlConnector.MySqlConnection connection = new MySqlConnector.MySqlConnection(comm.connexionBDD()))
+            {
+                MySqlConnector.MySqlCommand command = new MySqlConnector.MySqlCommand(query, connection);
+                connection.Open();
+                MySqlConnector.MySqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    int id = reader.GetInt32(0);
+                    string name = reader.GetString(1);
+
+                    comboBox1.Items.Add(new ComboBoxItem(id, name));
+                }
+                reader.Close();
+            }
         }
     }
 }
